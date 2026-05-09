19c23685f5374312920227d35f595f5a3eb83698
facetar825 <facetar825@gmail.com>
2026-05-09 21:15:11 +0800

    начел работу Создал основные Сцены и папки

---
+++ app/планировщик-меню/Скрипты/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_create_menu_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	pass # Replace with function body.
