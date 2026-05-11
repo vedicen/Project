884765ca71554882b42fa23dbfcce8b9a40d0fce
facetar825 <facetar825@gmail.com>
2026-05-11 23:50:22 +0800

    добавил shopping_list тестовый

---
+++ app/планировщик-меню/Сцены/shopping_list.gd
@@ -0,0 +1,58 @@
+extends Control
+
+@onready var shopping_text: RichTextLabel = $VBoxContainer/shopping_text
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	generate_shopping_list()
+	pass # Replace with function body.
+
+
+func generate_shopping_list():
+	shopping_text.text = ""
+	shopping_text.fit_content = true 
+	
+	var ingredients_dict = {}
+	Database.db.query("""
+		SELECT * FROM dish
+	""")
+	var dishes = Database.db.query_result
+	for dish in dishes:
+		var dish_id = dish["id_dish"]
+		
+		Database.db.query("""
+			SELECT ingredient.ingredient_name,
+			ingredient.unit,
+			dish_ingredient.quantity
+			FROM dish_ingredient
+			
+			JOIN ingredient
+			ON ingredient.id_ingredient = dish_ingredient.id_ingredient
+			
+			WHERE dish_ingredient.id_dish = 
+		""" + str(dish_id))
+		var ingredients = Database.db.query_result
+		for ingredient in ingredients:
+			var name = ingredient["ingredient_name"]
+			var quantity = ingredient["quantity"]
+			var unit = ingredient["unit"]
+			if ingredients_dict.has(name):
+				ingredients_dict[name]["quantity"] += quantity
+			else:
+					
+				ingredients_dict[name] ={
+					"quantity": quantity,
+					"unit" : unit
+				}	
+	shopping_text.text += "СПИСОК ПОКУПОК\n\n"
+	for name in ingredients_dict:
+		var quantity = ingredients_dict[name]["quantity"]
+		var unit = ingredients_dict[name]["unit"]
+		
+		shopping_text.text += (
+			name + " - "
+			+ str(quantity)
+			+ " "
+			+ unit + "\n"
+		)
--- app/планировщик-меню/Сцены/week_menu.gd
+++ app/планировщик-меню/Сцены/week_menu.gd
@@ -1,6 +1,12 @@
 extends Control
 @onready var week_menu: Control = $"."
 @onready var menu_text: RichTextLabel = $VBoxContainer/menu_text
+var total_week_calories = 0.0
+var total_week_price = 0.0
+
+var total_week_proteins = 0.0
+var total_week_fats =0.0
+var total_week_carbs = 0.0
 
 
 func _ready():
@@ -29,6 +35,12 @@ func show_week_menu():
 		"Воскресенье"
 	]
 	for day in days:
+		var day_calories = 0.0 
+		var day_price = 0.0 
+		
+		var day_proteins = 0.0 
+		var day_fats = 0.0 
+		var day_carbs = 0.0
 		menu_text.text += day + "\n"
 		#Завтрак
 		Database.db.query("""
@@ -37,10 +49,20 @@ func show_week_menu():
 			ORDER BY RANDOM()
 			LIMIT 1
 		""")
+		
 		var breakfast = Database.db.query_result[0]
+		print(breakfast)
+		
 		
 		menu_text.text +="Завтрак: "
 		menu_text.text += breakfast["dish_name"] + "\n"
+		day_calories += breakfast.get("calories", 0.0)
+		day_price += breakfast.get("price", 0.0)
+		
+		day_proteins += breakfast.get("proteins", 0.0)
+		day_fats += breakfast.get("fats", 0.0)
+		day_carbs += breakfast.get("carbohydrates", 0.0)
+		print(breakfast)
 		#ОБЕД
 		Database.db.query("""
 			SELECT * FROM dish
@@ -51,6 +73,12 @@ func show_week_menu():
 		var lunch = Database.db.query_result[0]
 		menu_text.text +="Обед: "
 		menu_text.text += lunch["dish_name"] + "\n"
+		day_calories += lunch.get("calories", 0.0)
+		day_price += lunch.get("price", 0.0)
+		
+		day_proteins += lunch.get("proteins", 0.0)
+		day_fats += lunch.get("fats", 0.0)
+		day_carbs += lunch.get("carbohydrates", 0.0)
 		
 		Database.db.query("""
 			SELECT * FROM dish
@@ -61,4 +89,39 @@ func show_week_menu():
 		var dinner = Database.db.query_result[0]
 		menu_text.text +="Ужин: "
 		menu_text.text += dinner["dish_name"] + "\n"
+		day_calories += dinner.get("calories", 0.0)
+		day_price += dinner.get("price", 0.0)
+		
+		day_proteins += dinner.get("proteins", 0.0)
+		day_fats += dinner.get("fats", 0.0)
+		day_carbs += dinner.get("carbohydrates", 0.0)
+		menu_text.text += "\nКалорий: " + str(day_calories)
+		menu_text.text += "\nБелки: " + str(day_proteins)
+		menu_text.text += "\nЖиры: " + str(day_fats)
+		menu_text.text += "\nУглеводы : " + str(day_carbs)
+		menu_text.text += "\nСтоймость : " + str(day_price) + "Р \n\n"
+		total_week_calories += day_calories
+		total_week_price += day_price
+		
+		total_week_proteins += day_proteins
+		total_week_fats += day_fats
+		total_week_carbs += day_carbs
+	menu_text.text += "++++++++++++++++++++++\n"
+	
+	menu_text.text += "ИТОГ ЗА НЕДЕЛЮ\n"	
+	
+	menu_text.text += "Калории: "
+	menu_text.text += str(total_week_calories) + "\n"
+	
+	menu_text.text += "Белки: "
+	menu_text.text += str(total_week_proteins)+"\n"
+	
+	menu_text.text += "Жиры: "
+	menu_text.text += str(total_week_fats)+"\n"
+	
+	menu_text.text += "Углеводы: "
+	menu_text.text += str(total_week_carbs)+"\n"
+	
+	menu_text.text += "Стоймость: "
+	menu_text.text += str(total_week_price)+"\n"
 	
