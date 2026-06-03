6b8aa041c7d91f3f12fd406d824e66619fe3310b
BasedTree811 <facetar825@gmail.com>
2026-06-03 10:36:00 +0800

    Сделал нормальную анимицию

--- app/rogailk/Player/sript/player.gd
+++ app/rogailk/Player/sript/player.gd
@@ -1,24 +1,33 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-@onready var animation_player: AnimationPlayer = $skin/AnimationPlayer
 
+@onready var animation_player: AnimationPlayer = $skin/AnimationPlayer
+@onready var animation_tree: AnimationTree = $skin/AnimationTree
 @onready var camera_pivot: Node3D = $camera_pivot
+
 var mouse_sensitivity = 0.003
+
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+	animation_tree.active = true
+
+	print("=== READY ===")
+	print("AnimationTree: ", animation_tree)
+	print("Active: ", animation_tree.active)
+	print("RunValue: ", animation_tree.get("parameters/RunValue"))
+
 func _input(event):
 	if event is InputEventMouseMotion:
 
-		# Вращение персонажа влево-вправо
+		# Поворот персонажа
 		rotate_y(-event.relative.x * mouse_sensitivity)
 
-		# Вращение камеры вверх-вниз
+		# Поворот камеры
 		camera_pivot.rotate_x(-event.relative.y * mouse_sensitivity)
 
-		# Ограничение угла камеры
 		camera_pivot.rotation.x = clamp(
 			camera_pivot.rotation.x,
 			deg_to_rad(-80),
@@ -27,21 +36,34 @@ func _input(event):
 
 	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-func _physics_process(delta: float) -> void:
-	
+
+func _physics_process(delta):
+
+	# Гравитация
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	# Прыжок
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var input_dir := Input.get_vector("rigth", "left", "down","up")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	# Движение
+	var input_dir = Input.get_vector("rigth", "left", "down", "up")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	# Анимация
+	var run_value = 1.0 if input_dir.length() > 0 else 0.0
+
+	if animation_tree.get("parameters/RunValue/blend_amount") != run_value:
+		print("RunValue -> ", run_value)
+
+	animation_tree.set("parameters/RunValue/blend_amount", run_value)
 
 	move_and_slide()
---
+++ app/rogailk/Skil.gd
@@ -0,0 +1,6 @@
+extends Node
+var critikl_dmg = 0
+var bonus_hp = 0
+var bonus_dmg = 0
+var bonus_speed = 0
+var bonus_speed_attack = 0
