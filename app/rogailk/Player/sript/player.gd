f422a2b2cd47d1ef92bb23533bb4d772ce15da20
BasedTree811 <facetar825@gmail.com>
2026-06-10 19:40:19 +0800

    Revert "scas"
    This reverts commit 18c078ab5c6648a0be70233b8b7da6b1abd5a732.

--- app/rogailk/Player/sript/player.gd
+++ app/rogailk/Player/sript/player.gd
@@ -60,7 +60,7 @@ func _physics_process(delta):
 	var direction = ( transform.basis * Vector3(-input_dir.x, 0, -input_dir.y)
 ).normalized()
 
-	if input_dir.length() > 0:
+	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
